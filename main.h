d14430e293c5dbbb4e7e66fce249e67121c7f8d6
hacman <hacman@hacman-Vostro-200>
2012-11-14 22:37:32 +0000

    changed main to theoretically remove the bug where the thing just stays on forever.
    Signed-off-by: Bob Clough <bob@clough.me>

--- main.h
+++ main.h
@@ -2,9 +2,11 @@ void Control_Init(void);
 void Control_Task(void);
 void Control_Doorbell(void);
 
-volatile int frntdoor_on;
-volatile int innrdoor_on;
-volatile int buzzer_on;
-volatile int light_on;
+uint16_t frntdoor_on;
+uint16_t innrdoor_on;
+uint16_t buzzer_on;
+uint16_t light_on;
 
-volatile int doorbell_last_checked;
+uint16_t doorbell_last_checked;
+
+char buf[3];
