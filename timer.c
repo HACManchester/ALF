43d638306628727840d00da30ed8d01204bbef87
Bob Clough <bob@clough.me>
2014-09-20 21:41:39 +0100

    Commented what timer.c setup is doing

--- timer.c
+++ timer.c
@@ -5,12 +5,13 @@
 #include <avr/interrupt.h>
 #include "bitm.h"
 #include "timer.h"
+
 void timer_init()
 {
-    TCCR1B |= (1 << WGM12);
-    TIMSK1 |= (1 << OCIE1A);
-    OCR1A = TIMER_TGT;
-    TCCR1B |= ((1 << CS10) | (1 << CS11)); //Set prescale to /64
+    TCCR1B |= (1 << WGM12);                // target is OCR1A
+    TIMSK1 |= (1 << OCIE1A);               // enable timer1_compA ISR
+    OCR1A = TIMER_TGT;                     // target value of timer for 10ms tick
+    TCCR1B |= ((1 << CS10) | (1 << CS11)); // set prescale to /64
 }
 
 ISR(TIMER1_COMPA_vect)
